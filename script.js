6f2bf46e49760fa8891f794d3709cd95f01e3c13
jeanlaura <laura.50.jean@gmail.com>
2018-10-25 16:56:15 +0200

    Problème ajout de mot + timer bizarre + lettres s'affichent pas

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 /*plateau*/
 var dictionnary = ['GITHUB','MKDIR','RM','ADD','GIT','COMMIT','MERGE','CHECKOUT','BRANCH','MASTER','ORIGIN','PUSH','PULL','CLONE','REPOSITORY',
 'HTML','IMG','SCRIPT','BODY','HEAD','DIV','SPAN','STRONG','BOLD','BR','FONT','META','STYLE','TABLE','TITLE','LINK','CLASS','ID','URL','FOOTER','HEADER','BUTTON','FORM','INPUT','SECTION','HREF','SRC','TYPE','VALUE','CAROUSEL','HTTP','CSS','ANIMATION','COLOR','BACKGROUND','DISPLAY','NONE','BLOCK','FLEX','PADDING','MARGIN','HEIGHT','WIDTH','TOP','BOTTOM','LEFT','RIGHT','BORDER','FIXED','BLACK','WHITE','RED','GREEN','YELLOW','BLUE','PINK','GREY','ORANGE','BROWN','PURPLE','SIZE','PYTHON','PHP','JQUERY','JAVA','JAVASCRIPT','JAVAEE','WORDPRESS','BOOTSTRAP','CONTAINER','ROW','MAIN','COL','MEDIA','CARD','RESPONSIVE','NAV','NAVBAR','TEXT','ROUNDED','DROPDOWN','FLUID','BORDERLESS','DARK','LIGHT','WARNING','DANGER','SUCESS','PRIMARY','SECONDARY','ALERT','BADGE','ITEM','DRUPAL','ANGULARJS','SCOPE','FUNCTION','NUMBER','FILTER','AJAX','SQL','MYSQL','ORACLE','JSP','VAR','XML','FLASH','NODEJS','ASP','ANOUSONE','CMS','JOOMLA','MAGENTA','LAURA','ROBIN','LUC','YASSINE','PRESTASHOP','RUBY','SASS','LESS','CDN','PARALLAX']
+
+// YASSINE
+// alphabet en tableau
+alphabet = ['','a','a','b','c','d','e','e','f','g','h','i','i','j','k','l','m','n','o','o','p','q','r','s','t','u','u','v','w','x','y','z','{','{','}','}','-','-','(','(',')',')'];
+alphabetgenerate = [];
+var timeoutID;
+
+
 $('input').keypress(function(event){
   var keyCode = event.keyCode && event.which;
   var valeur = $('#input').val();
@@ -14,11 +22,7 @@ $('input').keypress(function(event){
 // };
 });
 
-// YASSINE
-// alphabet en tableau
-alphabet = ['','a','a','b','c','d','e','e','f','g','h','i','i','j','k','l','m','n','o','o','p','q','r','s','t','u','u','v','w','x','y','z','{','{','}','}','-','-','(','(',')',')'];
-alphabetgenerate = [];
-var timeoutID;
+
 
 $(document).ready(function() {
   $('#button').click(function(){
